5c79f77e12cf2791508caec859e660238ff9b84e
sanadharwish <sanadharwish@gmail.com>
2023-09-01 20:27:01 +0530

    try and catch in products context

--- myproject/products/product_context.py
+++ myproject/products/product_context.py
@@ -1,33 +1,33 @@
-from django.shortcuts import render,redirect
-from products.models import Category,product,Order,OrderItem,Address,Wishlist,cart
+from django.shortcuts import render, redirect
+from products.models import Category, Product, Order, OrderItem, Address, Wishlist, Cart
 from django.contrib.auth.models import User
-
+from django.db.utils import ProgrammingError  # Import the exception
 
 def counts(request):
-    
-    if request.user.is_authenticated :
-        wishlists=Wishlist.objects.filter(user=request.user)
-        wishlist_count=wishlists.count()
+    wishlist_count = 0
+    cart_count = 0
+    orderitem_count = 0  # Initialize these variables with default values
 
-        # order=OrderItem.objects.filter(user=request.user)
-        # orderitem_count=order.count()
+    try:
+        if request.user.is_authenticated:
+            # Try to retrieve data from the database
+            wishlists = Wishlist.objects.filter(user=request.user)
+            wishlist_count = wishlists.count()
 
-    
+            order = OrderItem.objects.filter(user=request.user)
+            orderitem_count = order.count()
 
-        Cart=cart.objects.filter(user=request.user)
-        cart_count=Cart.count()
-    else :
+            Cart = Cart.objects.filter(user=request.user)
+            cart_count = Cart.count()
+    except ProgrammingError as e:
+        # Handle the exception gracefully (e.g., log the error)
+        # You can add custom logic here, like providing default values or rendering an error page.
+        pass
 
-        wishlist_count=0
-        cart_count=0
-        # orderitem_count=0
-
-    contxt={
-        'wishlist_count':wishlist_count,
-        'cart_count':cart_count,
-        # 'orderitem_count':orderitem_count,
-       
+    contxt = {
+        'wishlist_count': wishlist_count,
+        'cart_count': cart_count,
+        'orderitem_count': orderitem_count,
     }
 
     return contxt
-
